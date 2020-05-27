ad87d2f7f068e8501404423607d3a5be8c20ad4a
AI Spawn <50590465+AI-Spawn@users.noreply.github.com>
2020-05-27 10:59:25 -0400

    Delete upload.bat

--- upload.bat
+++
@@ -1,5 +0,0 @@
-git init
-git config --local user.name "AI-Spawn"
-git config --local user.email "50590465+AI-Spawn@users.noreply.github.com"
-git add .
-git commit -m "reuploading with verified + privacy stuff"
